5e08bf37c64fb5cb5eec44da98967ef6247be186
nikita <nikitademedyuk1@gmail.com>
2020-12-04 13:36:23 +0300

    fix

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -54,73 +54,75 @@ public class Main {
             System.out.println("\n1 - Use SQL request\n2 - Use list of companies");
             System.out.print("Enter: ");
             int variantUse = in.nextInt();
-
-            if (variantUse == 1) {
-                String fileNameSQL = "/Users/nikita/Documents/University/Programming/Java/Hometask-6/Hometask-6/RequestSQL.txt";
-                findByRequestSQL(companyList, fileNameSQL);
-            }
-            if (variantUse == 2) {
-                System.out.print("\n\n");
-                System.out.println("1 - Find a company by it's short name\n2 - Find a company by it's industry\n3 - Find a company by it's activity\n4 - " +
-                        "Find a company by it's date of foundation\n5 - Find a company by the number of staff\n6 - See the list of companies\n7 - Exit");
-                System.out.print("Enter: ");
-                int variantChoise = in.nextInt();
-
-                if (variantChoise == 1) {
-                        System.out.print("\nShort name of company: ");
-                        String lineNameCompany = in.nextLine();
-                        findCompany(companyList, lineNameCompany);
-                        writer.write("Short name of company : " + lineNameCompany + "\n");
+            switch (variantUse) {
+                case 1 -> {
+                    String fileNameSQL = "/Users/nikita/Documents/University/Programming/Java/Hometask-6/Hometask-6/RequestSQL.txt";
+                    findByRequestSQL(companyList, fileNameSQL);
                 }
+                case 2 -> {
+                    System.out.print("\n\n");
+                    System.out.println("1 - Find a company by it's short name\n2 - Find a company by it's industry\n3 - Find a company by it's activity\n4 - " +
+                            "Find a company by it's date of foundation\n5 - Find a company by the number of staff\n6 - See the list of companies\n7 - Exit");
+                    System.out.print("Enter: ");
+                    int variantChoise = in.nextInt();
 
-                if (variantChoise == 2) {
-                        System.out.print("\nIndustry of company: ");
-                        String lineIndustryCompany = in.nextLine();
-                        findCompanyByIndustry(companyList, lineIndustryCompany);
-                        writer.write("Industry of company : " + lineIndustryCompany + "\n");
-                }
-
-                if (variantChoise == 3) {
-                        System.out.print("\nType of activity: ");
-                        String lineActivityCompany = in.nextLine();
-                        findCompanyByActivity(companyList, lineActivityCompany);
-                        writer.write("Activity of company : " + lineActivityCompany + "\n");
-                }
-
-                if (variantChoise == 4) {
-                        try {
-                            System.out.print("\nEnter the date of foundation (min): ");
-                            int dateFoundationCompanyMin = in.nextInt();
-                            System.out.print("\nEnter the date of foundation (max): ");
-                            int dateFoundationCompanyMax = in.nextInt();
-                            findCompanyByDateFoundation(companyList, dateFoundationCompanyMin, dateFoundationCompanyMax);
-                            writer.write("Date of foundation (min) : " + dateFoundationCompanyMin + "\nDate of foundation (max) : " + dateFoundationCompanyMax);
+                    switch (variantChoise) {
+                        case 1 -> {
+                            System.out.print("\nShort name of company: ");
+                            String lineNameCompany = in.nextLine();
+                            findCompany(companyList, lineNameCompany);
+                            writer.write("Short name of company : " + lineNameCompany + "\n");
                         }
-                        catch (Exception e) {
-                            System.out.print("Error: " + e.getMessage());
-                        }
-                }
 
-                if (variantChoise == 5) {
-                        try {
-                            System.out.print("\nEnter number of staff (min): ");
-                            int staffCompanyMin = in.nextInt();
-                            System.out.print("\nEnter number of staff (max): ");
-                            int staffCompanyMax = in.nextInt();
-                            findCompanyByStaff(companyList, staffCompanyMin, staffCompanyMax);
-                            writer.write("Number of staff (min) : " + staffCompanyMin + "\nNumber of staff (max) : " + staffCompanyMax);
-                        } catch (Exception e) {
-                            System.out.print("Error: " + e.getMessage());
+                        case 2 -> {
+                            System.out.print("\nIndustry of company: ");
+                            String lineIndustryCompany = in.nextLine();
+                            findCompanyByIndustry(companyList, lineIndustryCompany);
+                            writer.write("Industry of company : " + lineIndustryCompany + "\n");
                         }
-                }
 
-                if (variantChoise == 6) {
-                        for (int i = 0; i < stringListD.size(); ++i) {
-                            System.out.print("\n" + i + ")");
-                            for (int j = 0; j < stringListD.get(i).size(); ++j) {
-                                System.out.print(stringListD.get(i).get(j) + "  ");
+                        case 3 -> {
+                            System.out.print("\nType of activity: ");
+                            String lineActivityCompany = in.nextLine();
+                            findCompanyByActivity(companyList, lineActivityCompany);
+                            writer.write("Activity of company : " + lineActivityCompany + "\n");
+                        }
+
+                        case 4 -> {
+                            try {
+                                System.out.print("\nEnter the date of foundation (min): ");
+                                int dateFoundationCompanyMin = in.nextInt();
+                                System.out.print("\nEnter the date of foundation (max): ");
+                                int dateFoundationCompanyMax = in.nextInt();
+                                findCompanyByDateFoundation(companyList, dateFoundationCompanyMin, dateFoundationCompanyMax);
+                                writer.write("Date of foundation (min) : " + dateFoundationCompanyMin + "\nDate of foundation (max) : " + dateFoundationCompanyMax);
+                            } catch (Exception e) {
+                                System.out.print("Error: " + e.getMessage());
                             }
                         }
+
+                        case 5 -> {
+                            try {
+                                System.out.print("\nEnter number of staff (min): ");
+                                int staffCompanyMin = in.nextInt();
+                                System.out.print("\nEnter number of staff (max): ");
+                                int staffCompanyMax = in.nextInt();
+                                findCompanyByStaff(companyList, staffCompanyMin, staffCompanyMax);
+                                writer.write("Number of staff (min) : " + staffCompanyMin + "\nNumber of staff (max) : " + staffCompanyMax);
+                            } catch (Exception e) {
+                                System.out.print("Error: " + e.getMessage());
+                            }
+                        }
+
+                        case 6 -> {
+                            for (int i = 0; i < stringListD.size(); ++i) {
+                                System.out.print("\n" + i + ")");
+                                for (int j = 0; j < stringListD.get(i).size(); ++j) {
+                                    System.out.print(stringListD.get(i).get(j) + "  ");
+                                }
+                            }
+                        }
+                    }
                 }
             }
         }
